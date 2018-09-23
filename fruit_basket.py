cf323d5414e9c8a031004a89e139276d2bac7f8a
kgutierrezevolveacademy <kgutierrez@evolveacademy.io>
2018-09-23 02:57:00 -0400

    change to 5 strikes

--- fruit_basket.py
+++ fruit_basket.py
@@ -4,18 +4,20 @@
 #9-22-18
 
 fruit_basket = ['apple', 'banana', 'cherry', 'date', 'elderberry']
+i = 1
     
 print ("Please guess the fruit in the basket: ")
 guess = str(input())
 
-while not (guess in fruit_basket):
+while not (guess in fruit_basket) and i < 5:
     print ("Guess again: " )
     guess = str(input())
+    i += 1
 
-#if (guess in fruit_basket):
-print("You chose the right fruit! \n", guess, "is in the basket!")
-#else:
-#    print(guess, "is not in the fruit basket \n")
+if (guess in fruit_basket):
+    print("You chose the right fruit! \n", guess, "is in the basket! \n")
+else:
+    print(guess, "is not in the fruit basket \n")
 
 
 print ("Finished, Thanks for playing!")
