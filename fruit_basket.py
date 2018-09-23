5630c19a2152ec34d11f8b4806d15b5b0e6d8a09
kgutierrezevolveacademy <kgutierrez@evolveacademy.io>
2018-09-23 02:23:41 -0400

    modified script

--- fruit_basket.py
+++ fruit_basket.py
@@ -8,10 +8,14 @@ fruit_basket = ['apple', 'banana', 'cherry', 'date', 'elderberry']
 print ("Please guess the fruit in the basket: ")
 guess = str(input())
 
-if (guess in fruit_basket):
-    print("You chose the right fruit! \n", guess, "is in the basket!")
-else:
-    print(guess, "is not in the fruit basket \n")
+while not (guess in fruit_basket):
+    print ("Guess again: " )
+    guess = str(input())
+
+#if (guess in fruit_basket):
+print("You chose the right fruit! \n", guess, "is in the basket!")
+#else:
+#    print(guess, "is not in the fruit basket \n")
 
 
 print ("Finished, Thanks for playing!")
