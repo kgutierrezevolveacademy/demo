fff1be1363d5492a3338f6440d21e1adfec36b3e
kgutierrezevolveacademy <kgutierrez@evolveacademy.io>
2018-09-23 02:00:29 -0400

    added script

---
+++ fruit_basket.py
@@ -0,0 +1,17 @@
+#EvolveAcademy Lab: 31
+#fruit_basket()
+#Ken.Gutierrez
+#9-22-18
+
+fruit_basket = ['apple', 'banana', 'cherry', 'date', 'elderberry']
+    
+print ("Please guess the fruit in the basket: ")
+guess = str(input())
+
+if (guess in fruit_basket):
+    print("You chose the right fruit! \n", guess, "is in the basket!")
+else:
+    print(guess, "is not in the fruit basket \n")
+
+
+print ("Finished, Thanks for playing!")
